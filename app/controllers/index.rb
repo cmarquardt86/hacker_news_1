aac612483600e4d3aff105c0975a4c5b6b4ab957
Apprentice <apprentice@devbootcamp.com>
2013-07-24 16:54:26 -0700

    everything else...

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,7 @@
 enable :sessions
 
 get '/' do
-  if session[:user_id]
-    redirect "/users/#{session[:user_id]}"
-  end
+
   erb :index
 end
 
@@ -14,23 +12,44 @@ post "/users" do
   redirect "/users/#{user.id}"
 end
 
+get '/users/:id' do |id|
+  @user = User.find(id)
+  erb :"users/show"
+end
+
+get '/sessions/new' do
+  erb :"sessions/new"
+end
+
 post '/sessions' do
   user = User.find_by_email(params[:email])
   if user.password == params[:password]
     session[:user_id] = user.id
-    redirect "/users/#{user.id}"
+    redirect "/"
   else
-    erb :index
+    erb :"sessions/new"
   end
-end
-
-get '/users/:id' do |id|
-  @user = User.find(id)
-  erb :"users/show"
 end
 
 get '/sessions/end' do 
   session.clear
   redirect '/'
 end
+
+post "/posts" do 
+  Post.create(title: params[:title], body: params[:body], user_id: "#{session[:user_id]}")
+  redirect "/users/#{session[:user_id]}"
+end
+
+get "/posts/:id" do |id|
+  @post = Post.find(id)
+  @user = User.find(@post.user_id)
+  erb :"posts/show"
+end
+
+post "/comments" do
+  Comment.create(content: params[:content], user_id: "#{session[:user_id]}", post_id: params[:post_id])
+  redirect "/posts/#{params[:post_id]}"
+end
+
 
